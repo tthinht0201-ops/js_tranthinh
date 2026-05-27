e37f31eeb9250afc7648c995bc1e8eb72b698730
tthinht0201-ops <testthinht0201@gmail.com>
2026-05-27 21:34:11 +0700

    update

--- React-bai1/bai-1/src/App.jsx
+++ React-bai1/bai-1/src/App.jsx
@@ -1,35 +1,49 @@
 import './App.css'
+import Products from './component/Products'
 
-const Header =(props) => {
-  return <h1>Ten hoc vien {props.name} , age {props.age}, height {props.height}</h1>
-}
+// const Products = (props) => {
+//   return <h1>{props.product}, gia san pham {props.value}</h1>
+// }
+// function App() {
+
+// const hocsinh={
+//   name:"thinh",
+//   age: 90,
+//   height:"1m78"
+// }
+
+// const handleClick = () =>{
+//   const a=5;
+//   const b=3;
+//   console.log(a+b)
+// }
 
 
-const Products = (props) => {
-  return <h1>{props.product}, gia san pham {props.value}</h1>
-}
+//  return (
+//    <div>
+//      <Products product={"Laptop"} value={500000000} />
+//      <Products product={"smartphone"} value={76700000000} />
+//      <Header name={hocsinh.name} age={hocsinh.age} height={hocsinh.height} />
+//      <button onClick={handleClick}> Click me</button>
+//    </div>
+//  )
+//}
+
 function App() {
-
-  const hocsinh={
-    name:"thinh",
-    age: 90,
-    height:"1m78"
-  }
-
-  const handleClick = () =>{
-    const a=5;
-    const b=3;
-    console.log(a+b)
-  }
-
+  const products = [
+    { product: "iPHONE 14 PRO MAX", value: 1000 },
+    { product: "MacBook Pro 16 inch", value: 2500 },
+    { product: "Apple Watch series 7", value: 500 }
+  ]
 
   return (
-    <div>
-      <Products product={"Laptop"} value={500000000} />
-      <Products product={"smartphone"} value={76700000000} />
-      <Header name={hocsinh.name} age={hocsinh.age} height={hocsinh.height} />
-      <button onClick={handleClick}> Click me</button>
-    </div>
+    <>
+      {
+        products.map((item) => {
+        return <Products name={item.product} value={item.value} />
+      })
+    }
+    </>
   )
 }
 
---
+++ React-bai1/bai-1/src/component/Products.jsx
@@ -0,0 +1,5 @@
+const Products =(props) => {
+  return <h1> {props.name}  {props.value}vnd </h1>
+}
+
+export default Products;
