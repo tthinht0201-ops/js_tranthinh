ecc505eb83e6ed57e122274c3c6f9e9669061704
tthinht0201-ops <testthinht0201@gmail.com>
2026-05-27 21:05:42 +0700

    update

--- React-bai1/bai-1/src/App.jsx
+++ React-bai1/bai-1/src/App.jsx
@@ -1,13 +1,34 @@
-const Header =() => {
-  return <h1>Trung Tam Tin Hoc</h1>
+import './App.css'
+
+const Header =(props) => {
+  return <h1>Ten hoc vien {props.name} , age {props.age}, height {props.height}</h1>
 }
 
+
+const Products = (props) => {
+  return <h1>{props.product}, gia san pham {props.value}</h1>
+}
 function App() {
+
+  const hocsinh={
+    name:"thinh",
+    age: 90,
+    height:"1m78"
+  }
+
+  const handleClick = () =>{
+    const a=5;
+    const b=3;
+    console.log(a+b)
+  }
+
+
   return (
     <div>
-      <Header />
-      <Header />
-      <Header />
+      <Products product={"Laptop"} value={500000000} />
+      <Products product={"smartphone"} value={76700000000} />
+      <Header name={hocsinh.name} age={hocsinh.age} height={hocsinh.height} />
+      <button onClick={handleClick}> Click me</button>
     </div>
   )
 }
