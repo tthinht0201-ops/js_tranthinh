1fe8893c425a6fc1087c83fc3bd38c881bffba07
tthinht0201-ops <testthinht0201@gmail.com>
2026-05-27 19:52:21 +0700

    update

---
+++ React-bai1/bai-1/src/App.jsx
@@ -0,0 +1,15 @@
+const Header =() => {
+  return <h1>Trung Tam Tin Hoc</h1>
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Header />
+      <Header />
+    </div>
+  )
+}
+
+export default App
